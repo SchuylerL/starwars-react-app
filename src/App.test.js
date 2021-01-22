6bd1763e824685b0de1aae97cf11169c81c0b90d
skythrilla <schuyler.lawrence@gmail.com>
2021-01-22 01:09:47 -0800

    test swapi

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
+test('renders star wars', () => {
   const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
+  // const linkElement = getByText(/Hide Details/i);
   const linkElement = getByText(/Star Wars/);
   expect(linkElement).toBeInTheDocument();
 });
-// test('renders react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/1/i);
-//   // const linkElement = getByText(/Star Wars/);
-//   expect(linkElement).toBeInTheDocument();
-// });
+
+describe('/GET /api/people', () => {
+  it('serves up all people', async () => {
+    const domain = 'https://swapi.dev/api/people/?page=1';
+    const response = await axios.get(domain);
+    expect(response.status).toEqual(200);
+  });
+});
+
