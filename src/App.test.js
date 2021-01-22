7d93d38128c43c98f81d791702d652f06b81e702
skythrilla <schuyler.lawrence@gmail.com>
2021-01-21 23:37:51 -0800

    link test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  // const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Star Wars/);
   expect(linkElement).toBeInTheDocument();
 });
+// test('renders react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/1/i);
+//   // const linkElement = getByText(/Star Wars/);
+//   expect(linkElement).toBeInTheDocument();
+// });
