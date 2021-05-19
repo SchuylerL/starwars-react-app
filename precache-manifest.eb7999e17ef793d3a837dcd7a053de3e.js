405c0a68b0e3847ff49462bad7e9889e4fee2ed0
skythrilla <schuyler.lawrence@gmail.com>
2021-05-19 15:27:51 -0700

    Updates

---
+++ precache-manifest.eb7999e17ef793d3a837dcd7a053de3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6231aba4af72947d556ad4cba46217d",
+    "url": "/starwars-react-app/index.html"
+  },
+  {
+    "revision": "286fa30157f0741be475",
+    "url": "/starwars-react-app/static/css/main.2b976c83.chunk.css"
+  },
+  {
+    "revision": "d1596662be4bbff06414",
+    "url": "/starwars-react-app/static/js/2.726613e3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-react-app/static/js/2.726613e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286fa30157f0741be475",
+    "url": "/starwars-react-app/static/js/main.d5671ccd.chunk.js"
+  },
+  {
+    "revision": "988fb04e426cdbbd1b6e",
+    "url": "/starwars-react-app/static/js/runtime-main.4e879e60.js"
+  },
+  {
+    "revision": "9180d122304038270fbd72553b583a48",
+    "url": "/starwars-react-app/static/media/deathstar.9180d122.png"
+  }
+]);
